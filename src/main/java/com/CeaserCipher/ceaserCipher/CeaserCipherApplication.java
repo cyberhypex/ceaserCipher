4cf818d9763470d3ca74b4ae50ea2ebde94d1b88
Anshuman <chaoanxuman999@gmail.com>
2025-06-06 23:15:58 +0530

    ceaserCipher

---
+++ src/main/java/com/CeaserCipher/ceaserCipher/CeaserCipherApplication.java
@@ -0,0 +1,13 @@
+package com.CeaserCipher.ceaserCipher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CeaserCipherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CeaserCipherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CeaserCipher/ceaserCipher/CeaserCipherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CeaserCipher.ceaserCipher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CeaserCipherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
