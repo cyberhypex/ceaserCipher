a2ca323b3418950ccff93d556b23dbbf30dc4770
Anshuman <chaoanxuman999@gmail.com>
2025-06-28 23:56:35 +0530

    Testing frontend

--- src/main/java/com/CeaserCipher/ceaserCipher/Configuration/CorsConfig.java
+++ src/main/java/com/CeaserCipher/ceaserCipher/Configuration/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
         @Override
             public void addCorsMappings(CorsRegistry corsRegistry){
         corsRegistry.addMapping("/**")
-                .allowedOrigins("https://ceaser-cipher-frontend.vercel.app/")
+                .allowedOrigins("http://localhost:5174/")
                 .allowedMethods("GET","POST", "PUT", "DELETE")
                 .allowedHeaders("*");
         }
